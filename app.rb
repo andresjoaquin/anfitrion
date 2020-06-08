2e6a3f9ab767fad8711b8258e08096c0865a14d4
andresjoaquin <andresjoaquin@gmail.com>
2020-06-08 07:28:58 +0000

    Primer versión del anfitrion intregado Sinatra

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'sinatra'
 require './config'
+require './lib/anfitrion'
 
 get '/' do
-    'hello world'
+    erb :hola
+end
+
+post '/saluda' do
+    anfitrion= Anfitrion.new params['idioma']
+    @mensaje= anfitrion.saluda params['nombre']
+    erb :saluda
 end
